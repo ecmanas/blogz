93ec7e5a6fefdc099fbfce56fc8e266f889b0297
Eleni Anas <ecmanas@gmail.com>
2017-10-12 19:21:36 -0500

    final code

--- main.py
+++ main.py
@@ -18,6 +18,21 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
+@app.route('/singlepost', methods=['POST', 'GET'])
+def singlepost():
+    
+    id = request.args.get('id')
+
+    #use id to query database save to variable
+
+    post = Blog.query.filter_by(id=id).first()
+    #blogpost = #query return using id
+
+
+    #render template with variable 
+    return render_template('singlepost.html', post=post)
+
+    
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
 
@@ -25,24 +40,34 @@ def newpost():
         title = request.form['title']
         body = request.form['body']
         new_post = Blog(title, body)
+        
+        title = str(request.form['title'])
+        title_from_form = title
+        title_error = ''
+
+        body = str(request.form['body'])
+        body_from_form = body
+        body_error = ''
+
+        if title_from_form == "":
+            title_error = "please enter valid title"
+        if body_from_form == "":
+            body_error = "please enter valid body"  
+            return render_template('newpost.html', title_error = title_error, body_error = body_error)
+        
         db.session.add(new_post)
         db.session.commit()
-        return redirect('/singlepost')
-
-    title_error = ''
-    body_error = ''
+        id = new_post.id
+        return redirect('/singlepost?id={}'.format(id))
 
     return render_template('newpost.html')
+    
 
 @app.route('/blogposts', methods=['POST', 'GET'])
 def blog():
+    
     blogs = Blog.query.all()
     return render_template('blogposts.html', blogs=blogs)
-
-@app.route('/singlepost')
-def singlepost():
-    blog_id = request.args.get('id')
-    return render_template('singlepost.html')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
